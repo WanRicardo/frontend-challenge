18ac1698641d1c77d1ad2d91e9aaa6b789503ec7
WanRicardo <wanderson.silva@gec.inatel.br>
2019-01-25 01:59:02 -0200

    Commit desafio

---
+++ js/script.js
@@ -0,0 +1,8 @@
+//função do botão menu ao ser clicado
+$(".btn-menu").click(function(){
+	$(".menu").show();
+});
+//função do botão fechar ao ser clicado
+$(".btn-close").click(function(){
+	$(".menu").hide();
+});
